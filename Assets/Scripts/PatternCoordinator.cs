052446674074740d4573373ec4de9c22d3319b8f
hubatish <zachariah.howell@gmail.com>
2015-07-01 20:22:24 -0400

    At least patterns are generated

--- Assets/Scripts/PatternCoordinator.cs
+++ Assets/Scripts/PatternCoordinator.cs
@@ -11,26 +11,28 @@ public class PatternCoordinator : MonoBehaviour
 {
     public static PatternCoordinator Instance;
 
-    public ElementManager elementManager;
+    protected ElementManager elementManager;
     protected void Start()
     {
         elementManager = gameObject.GetComponent<ElementManager>();
-        GeneratePattern(5);
+        GeneratePattern(defaultNumElements);
         Instance = this;
     }
 
     public List<Element> spawnedElements = new List<Element>();
     public float distBetweenElements = 0.75f;
+    public int defaultNumElements = 5;
 
     public void GeneratePattern(int length)
     {
         for (int i = 0; i < length; i++)
         {
             Element newElement = (Element) GameObject.Instantiate(elementManager.GetRandomElement(), 
-                                transform.position + distBetweenElements * i * Vector3.down,
-                                                                        Quaternion.identity);
+                                                                            GetVector3FromPosition(i),
+                                                                                Quaternion.identity);
             spawnedElements.Add(newElement);
         }
+        currentPosition = 0;
     }
 
     public void ClearPattern()
@@ -41,5 +43,36 @@ public class PatternCoordinator : MonoBehaviour
         }
     }
 
+    protected Vector3 GetVector3FromPosition(int elementNum)
+    {
+        return transform.position + distBetweenElements * elementNum * Vector3.down;
+    }
 
+    //manage current position
+    public int currentPosition
+    {
+        get
+        {
+            return _currentPosition;
+        }
+        set
+        {
+            _currentPosition = value;
+            indicator.position = GetVector3FromPosition(value) + Vector3.left*distBetweenElements;
+        }
+    }
+    private int _currentPosition;
+
+    public Transform indicator;
+
+    public void AdvancePosition()
+    {
+        currentPosition += 1;
+        if (currentPosition > spawnedElements.Count-1)
+        {
+            //won the pattern!
+            ClearPattern();
+            GeneratePattern(defaultNumElements);
+        }
+    }
 }
